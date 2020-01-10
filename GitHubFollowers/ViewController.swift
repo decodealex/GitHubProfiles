25f1aacf2d2060cd3a24d722f187dd576338e362
Alex <oleksandr>
2020-01-10 11:29:53 +0200

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Oleksandr Kovalyshyn on 10.01.2020.
+//  Copyright © 2020 Oleksandr Kovalyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
